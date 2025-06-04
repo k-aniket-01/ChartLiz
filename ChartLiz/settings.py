a178c71a837a983a5f5fac75a506836f4261ee38
k-aniket-01 <khomaneaniket9420@gmail.com>
2025-06-04 15:18:32 +0530

    in

--- ChartLiz/settings.py
+++ ChartLiz/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_!3xhubkcv5=k0sc&u8@18v5=fx=u=qay=vkviwvvahmjbcrgo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ChartLiz.urls'
@@ -118,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR, "static"
