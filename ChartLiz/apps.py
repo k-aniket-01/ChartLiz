58d4b14b8d1b7e456f7057806e8e862e89ed7ae5
aniket-consociate <aniket.khomane@consociatesolutions.com>
2026-03-18 17:05:50 +0530

    [fix] cleaned v2 branch (delted all )

--- ChartLiz/apps.py
+++
@@ -1,9 +0,0 @@
-# apps.py
-from django.apps import AppConfig
-
-class YourAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ChartLiz'
-
-    def ready(self):
-        import ChartLiz.signals  # make sure this line is here
