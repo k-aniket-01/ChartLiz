e992b63215a60ddc7b189bd265f637880ebcd88e
aniket-consociate <aniket.khomane@consociatesolutions.com>
2026-03-26 12:40:20 +0000

    [fix] asgi app registry issue

--- chartliz/asgi.py
+++ chartliz/asgi.py
@@ -1,19 +1,12 @@
-import os
-from django.core.asgi import get_asgi_application
+from django.urls import path
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import stocks.routing
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chartliz.settings')
 
-from stocks.routing import websocket_urlpatterns as stocks_ws
-from alerts.routing import websocket_urlpatterns as alerts_ws
-
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
-    'websocket': AuthMiddlewareStack(
-        URLRouter(
-            stocks_ws + alerts_ws
-        )
+    "http": get_asgi_application(),
+    "websocket": URLRouter(
+        __import__("stocks.routing").routing.websocket_urlpatterns
     ),
 })
